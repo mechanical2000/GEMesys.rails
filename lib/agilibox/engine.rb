f3459fb5d32fcfccc9ab65c30415b9f4b1ad709c
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2017-02-28 08:37:47 +0100

    Change engine requires order

--- lib/agilibox/engine.rb
+++ lib/agilibox/engine.rb
@@ -1,9 +1,9 @@
 require "rails-i18n"
 
-require_relative "core_and_rails_ext"
-
 module Agilibox
   class Engine < ::Rails::Engine
     isolate_namespace Agilibox
   end
 end
+
+require_relative "core_and_rails_ext"
