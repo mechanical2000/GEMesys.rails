5c3263d99ccbc2a27be928951bd626653fa8bb0a
Benoit MARTIN-CHAVE <benoit@martin-chave.ch>
2017-05-30 12:07:57 +0200

    Add mime types

--- lib/agilibox/engine.rb
+++ lib/agilibox/engine.rb
@@ -3,6 +3,11 @@ require "rails-i18n"
 module Agilibox
   class Engine < ::Rails::Engine
     isolate_namespace Agilibox
+
+    initializer "agilibox_mime_types" do
+      Mime::Type.register "application/vnd.ms-excel", :xls
+      Mime::Type.register "application/vnd.ms-excel", :xlsx
+    end
   end
 end
 
