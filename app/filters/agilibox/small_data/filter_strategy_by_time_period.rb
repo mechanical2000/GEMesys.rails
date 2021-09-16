a3b4ecc962cdd7c2ebb614cd86e24c64d6a40d2a
Benoit MARTIN-CHAVE <benoit@martin-chave.com>
2021-09-16 14:58:12 +0200

    Remove deprecated filter

--- app/filters/agilibox/small_data/filter_strategy_by_time_period.rb
+++
@@ -1,6 +0,0 @@
-class Agilibox::SmallData::FilterStrategyByTimePeriod
-  def initialize(*)
-    raise "FilterStrategyByTimePeriod is deprecated, please use " \
-      "FilterStrategyByDatePeriod or FilterStrategyByDatetimePeriod"
-  end
-end
